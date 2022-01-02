cdb194e73622fb7844599d793415c0e58f60017d
kim-siot <qiooiu@naver.com>
2022-01-02 18:38:55 +0900

    UI_Design_and_Comments

--- app.js
+++ app.js
@@ -20,26 +20,50 @@ function newsFeed() {
   const newsFeed = getData(NEWS_URL);
   const newsList = [];
   let template = `
-    <div class="container mx-auto p-4">
-      <h1>Hacker News</h1> 
-      <ul>
-        {{__news_feed__}}
-      </ul>
-    </div>
-    <div>
-      <a href="#/page/{{__prev_page__}}">Prev</a>
-      <a href="#/page/{{__next_page__}}">Next</a>
+    <div class="bg-gray-600 min-h-screen">
+      <div class="bg-white text-xl">
+        <div class="mx-auto px-4">
+          <div class="flex justify-between items-center py-6">
+            <div class="flex justify-start">
+              <h1 class="font-extrabold">Hacker News</h1>
+            </div>
+            <div class="items-center justify-end">
+              <a href="#/page/{{__prev_page__}}" class="text-gray-500">
+                Previous
+              </a>
+              <a href="#/page/{{__next_page__}}" class="text-gray-500 ml-4">
+                Next
+              </a>
+            </div>
+          </div> 
+        </div>
+      </div>
+      <div class="p-4 text-2xl text-gray-700">
+        {{__news_feed__}}        
+      </div>
     </div>
   `;
 
 
   for(let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
     newsList.push(`
-      <li>
-        <a href="#/show/${newsFeed[i].id}">
-          ${newsFeed[i].title} (${newsFeed[i].comments_count})
-        </a>
-      </li>
+      <div class="p-6 ${newsFeed[i].read ? 'bg-red-500' : 'bg-white'} mt-6 rounded-lg shadow-md transition-colors duration-500 hover:bg-green-100">
+        <div class="flex">
+          <div class="flex-auto">
+            <a href="#/show/${newsFeed[i].id}">${newsFeed[i].title}</a>  
+          </div>
+          <div class="text-center text-sm">
+            <div class="w-10 text-white bg-green-300 rounded-lg px-0 py-2">${newsFeed[i].comments_count}</div>
+          </div>
+        </div>
+        <div class="flex mt-3">
+          <div class="grid grid-cols-3 text-sm text-gray-500">
+            <div><i class="fas fa-user mr-1"></i>${newsFeed[i].user}</div>
+            <div><i class="fas fa-heart mr-1"></i>${newsFeed[i].points}</div>
+            <div><i class="far fa-clock mr-1"></i>${newsFeed[i].time_ago}</div>
+          </div>  
+        </div>
+      </div>    
     `);
   }
 
@@ -53,15 +77,59 @@ function newsFeed() {
 
 function newsDetail() {
   const id = location.hash.substr(7);
-  
   const newsContent = getData(CONTENT_URL.replace('@id', id));
+  let template = `
+    <div class="bg-gray-600 min-h-screen pb-8">
+      <div class="bg-white text-xl">
+        <div class="mx-auto px-4">
+          <div class="flex justify-between items-center py-6">
+            <div class="flex justify-start">
+              <h1 class="font-extrabold">Hacker News</h1>
+            </div>
+            <div class="items-center justify-end">
+              <a href="#/page/${store.currentPage}" class="text-gray-500">
+                <i class="fa fa-times"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
 
-  container.innerHTML = `
-    <h1>${newsContent.title}</h1>
-    <div>
-      <a href="#/page/${store.currentPage}">목록으로</a>
+      <div class="h-full border rounded-xl bg-white m-6 p-4 ">
+        <h2>${newsContent.title}</h2>
+        <div class="text-gray-400 h-20">
+          ${newsContent.content}
+        </div>
+
+        {{__comments__}}
+
+      </div>
     </div>
   `;
+
+  function makeComment(comments, called = 0) {
+    const commentString = [];
+
+    for(let i = 0; i < comments.length; i++) {
+      commentString.push(`
+      <div style="padding-left: ${called * 40}px;" class="mt-4">
+        <div class="text-gray-400">
+          <i class="fa fa-sort-up mr-2"></i>
+          <strong>${comments[i].user}</strong> ${comments[i].time_ago}
+        </div>
+        <p class="text-gray-700">${comments[i].content}</p>
+      </div>    
+      `);
+
+      if(comments[i].comments.length > 0) {
+        commentString.push(makeComment(comments[i].comments, called + 1)); //재귀함수
+      }
+    }
+
+    return commentString.join('');
+  }
+
+  container.innerHTML = template.replace('{{__comments__}}', makeComment(newsContent.comments));
 }
 
 
