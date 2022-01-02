7364203895b8bc9021d402aaa569b43bd93caac5
kim-siot <qiooiu@naver.com>
2022-01-02 17:40:34 +0900

    paging

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const ajax = new XMLHttpRequest();
 const content = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
+const store = {
+  currentPage: 1,
+}
+
 
 function getData(url) {
   ajax.open('GET', url, false);
@@ -18,10 +22,10 @@ function newsFeed() {
 
   newsList.push('<ul>');
 
-  for(let i=0; i < 10; i++) {
+  for(let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
     newsList.push(`
       <li>
-        <a href="#${newsFeed[i].id}">
+        <a href="#/show/${newsFeed[i].id}">
           ${newsFeed[i].title} (${newsFeed[i].comments_count})
         </a>
       </li>
@@ -29,30 +33,44 @@ function newsFeed() {
   }
 
   newsList.push('</ul>');
+  newsList.push(`
+    <div>
+      <a href="#/page/${store.currentPage > 1 ? store.currentPage - 1 : 1}">Prev</a>
+      <a href="#/page/${store.currentPage + 1}">Next</a>
+    </div>
+  `);
 
   container.innerHTML = newsList.join('');
 }
 
 
 function newsDetail() {
-  const id = location.hash.substr(1);
+  const id = location.hash.substr(7);
   
   const newsContent = getData(CONTENT_URL.replace('@id', id));
-  const title = document.createElement('h1');
 
   container.innerHTML = `
     <h1>${newsContent.title}</h1>
     <div>
-      <a href= "#">목록으로</a>
+      <a href="#/page/${store.currentPage}">목록으로</a>
     </div>
   `;
 }
 
 function router() {
-  newsFeed();
+  const routePath = location.hash;
+
+  if (routePath === '') {
+    newsFeed();
+  } else if(routePath.indexOf('#/page/') >= 0 ){
+    store.currentPage = Number(routePath.substr(7));
+    newsFeed();
+  } else {
+    newsDetail();
+  }
 }
 
-window.addEventListener('hashchange', newsDetail);
+window.addEventListener('hashchange', router);
 
-router()
+router();
 
