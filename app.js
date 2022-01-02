271f00b36e3a310c95ce862f11ac2c7b9cf20db0
kim-siot <qiooiu@naver.com>
2022-01-02 16:17:50 +0900

    router_start

--- app.js
+++ app.js
@@ -4,39 +4,55 @@ const content = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
 
-
-ajax.open('GET', NEWS_URL, false);
-ajax.send();
-
-const newsFeed = JSON.parse(ajax.response);
-const ul = document.createElement('ul');
-
-window.addEventListener('hashchange', function() {
-  const id = location.hash.substr(1);
-
-  ajax.open('GET', CONTENT_URL.replace('@id', id), false);
+function getData(url) {
+  ajax.open('GET', url, false);
   ajax.send();
 
-  const newsContent = JSON.parse(ajax.response);
-  const title = document.createElement('h1');
-
-  title.innerHTML = newsContent.title;
-  content.appendChild(title);
-
-  console.log(newsContent.title);
-});
-
-for(let i=0; i < 10; i++) {
- const li = document.createElement('li');
- const a = document.createElement('a');
-
- a.href = `#${newsFeed[i].id}`;
- a.innerHTML = `${newsFeed[i].title} (${newsFeed[i].comments_count})`;
-
- li.appendChild(a);
- ul.appendChild(li);
-
+  return JSON.parse(ajax.response);
 }
 
-container.appendChild(ul);
-container.appendChild(content);
+
+function newsFeed() {
+  const newsFeed = getData(NEWS_URL);
+  const newsList = [];
+
+  newsList.push('<ul>');
+
+  for(let i=0; i < 10; i++) {
+    newsList.push(`
+      <li>
+        <a href="#${newsFeed[i].id}">
+          ${newsFeed[i].title} (${newsFeed[i].comments_count})
+        </a>
+      </li>
+    `);
+  }
+
+  newsList.push('</ul>');
+
+  container.innerHTML = newsList.join('');
+}
+
+
+function newsDetail() {
+  const id = location.hash.substr(1);
+  
+  const newsContent = getData(CONTENT_URL.replace('@id', id));
+  const title = document.createElement('h1');
+
+  container.innerHTML = `
+    <h1>${newsContent.title}</h1>
+    <div>
+      <a href= "#">목록으로</a>
+    </div>
+  `;
+}
+
+function router() {
+  newsFeed();
+}
+
+window.addEventListener('hashchange', newsDetail);
+
+router()
+
